41cb4b9f4f8bb9dca3427ac1b74bb25f25c13354
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2024-08-04 20:28:05 +0900

    wip: html

--- main.py
+++ main.py
@@ -11,16 +11,28 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 raise Exception('Content-Length header is missing')
 
             content_length = int(contentLength)
+
+            # Input type is always application/pdf
+            # determine the output format from the Accept header
+            # if Accept header is not present, default to text/plain
+            format = 'text'
+            if 'Accept' in self.headers:
+                if 'text/plain' in self.headers['Accept']:
+                    format = 'text'
+                elif 'text/html' in self.headers['Accept']:
+                    format = 'html'
+
             post_data = self.rfile.read(content_length)
 
             pdf_file = post_data.decode('utf-8')
 
-            text = extract_text(pdf_file)
+            outfp = StringIO()
+            extract_text_to_fp(pdf_file, output_type=format, outfp=outfp)
 
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(text.encode())
+            self.wfile.write(outfp.getvalue().encode())
         except Exception as e:
             self.send_response(400)
             self.send_header('Content-type', 'text/plain')
